7817d519f78bf43683b6f38b5af419f0e60f0e20
offinliu <liu957muyao@gmail.com>
2021-10-07 20:44:05 +0800

    win patch
    Windows compatibility patch

---
+++ scripts/build_app_shape_database_WINDOWS.ps1
@@ -0,0 +1,15 @@
+Write-Host "INFO: This script assumes that you are running from the \"scripts\" folder."
+Write-Host "Creating..."
+
+mkdir -p build/bin
+cd build
+
+Write-Host "Compiling..."
+gcc -I ../include/ -c ../app/app_shape_database.c
+gcc -I ../include/ -c ../src/menu_components.c
+gcc -I ../include/ -c ../src/linked_list_struct.c
+gcc -I ../include/ -c ../src/graphical.c
+Write-Host "Linking..."
+gcc app_shape_database.o menu_components.o linked_list_struct.o graphical.o -o ./bin/app_main -lm
+Write-Host "Executing..."
+.\bin\app_main.exe
